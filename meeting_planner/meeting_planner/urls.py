7b66f98bf158d511d69099d0e37362ffb5dba6de
Kevin <kevin.peraza@live.com>
2021-04-01 21:19:35 -0600

    Update urls

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from website.views import welcome, date, about
+from website.views import welcome
+from meetings.views import detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome),
-    path('date', date),
-    path('about', about)
+    path('meetings/<int:id>', detail),
 ]
