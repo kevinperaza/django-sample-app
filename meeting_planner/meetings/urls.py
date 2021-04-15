20e5d727f5f8aff81dcdb72182aa82fb85fd685f
Kevin <kevin.peraza@live.com>
2021-04-14 20:38:45 -0600

    Cleanup routes

--- meeting_planner/meetings/urls.py
+++ meeting_planner/meetings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetings/<int:id>', views.meeting_details, name="meeting_details"),
+    path('<int:id>', views.meeting_details, name="meeting_details"),
     path('rooms', views.rooms_list, name="rooms_list"),
     path('rooms/<int:id>', views.room_details, name="room_details"),
 
